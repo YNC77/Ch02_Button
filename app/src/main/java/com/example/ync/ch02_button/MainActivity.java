81a96f2adf3d7615d6b418dee697cc235adf188d
YNC <ginger2033@gmail.com>
2017-05-11 16:22:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ync/ch02_button/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.ync.ch02_button;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    int size = 30;
+    public void bigger(View v){
+        TextView txv;
+        txv = (TextView) findViewById(R.id.txv);
+        txv.setTextSize(++size);
+    }
+    public void smaller(View v){
+        if(size>30) {
+            TextView txv = (TextView) findViewById(R.id.txv);
+            txv.setTextSize(--size);
+        }
+
+    }
+}
